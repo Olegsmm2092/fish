5dce97861ee8e345ed0a19d96e4b3bd09fcf6f56
Olegsmm2092 <radoveev@gmail.com>
2025-03-04 13:26:28 +0800

    Write first pattern:proxy to oop

--- oop/oop_pattern_proxy.py
+++ oop/oop_pattern_proxy.py
@@ -1,11 +1,11 @@
 class BankAccount:
-    def __init__(self,owner=0,balance=None):
+    def __init__(self,owner,balance=0):
         self.owner = owner
         self.balance = balance
 
     def deposit(self,amount):
-        if amout > 0:
-            self.balance += 1
+        if amount > 0:
+            self.balance += amount
 
     def withdraw(self,amount):
         if 0 < amount <= self.balance:
@@ -16,3 +16,21 @@ class BankAccount:
     def get_balance(self):
         return self.balance
 
+class ProxyBankAccount:
+    def __init__(self,owner,balance=0):
+        self.bank_account = BankAccount(owner, balance)
+
+    def deposit(self,amount):
+        self.bank_account.deposit(amount)
+    
+    def withdraw(self,amount):
+        self.bank_account.withdraw(amount)
+
+    def get_balance(self):
+       return self.bank_account.get_balance()
+
+proxy = ProxyBankAccount("John Doe", 1000)
+proxy.deposit(500)
+proxy.withdraw(200)
+print(f"Current balance: {proxy.get_balance()}")
+    
